737d035be81cf60227fe039acecfb3a776de358d
henghengyh <yihenglee6@gmail.com>
2026-01-22 16:56:01 +0800

    [Level 3] Mark as Done

--- src/main/java/Agnes.java
+++ src/main/java/Agnes.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Agnes {
-    private List<String> items = new ArrayList<>();
+    private List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         Agnes myBot = new Agnes();
@@ -14,6 +14,7 @@ public class Agnes {
         endConversation();
     }
 
+    // DEFAULT CONVERSATIONS
     private void startConversation() {
         printReply(
                 "Hello! I'm " + Agnes.class.getName(),
@@ -25,6 +26,7 @@ public class Agnes {
         printReply("Goodbye! Have a wonderful day ahead!");
     }
 
+    // USER INPUT
     private void userInput() {
         Scanner sc = new Scanner(System.in);
         while (true) {
@@ -33,18 +35,41 @@ public class Agnes {
                 break;
             } else if (request.equals("list")) {
                 listItems();
+            } else if (request.split(" ")[0].equals("mark")) {
+                int taskNo = Integer.parseInt(request.split(" ")[1]);
+                markTask(tasks.get(taskNo - 1), true);
+            } else if (request.split(" ")[0].equals("unmark")) {
+                int taskNo = Integer.parseInt(request.split(" ")[1]);
+                markTask(tasks.get(taskNo - 1), false);
             } else {
-                items.add(request);
+                tasks.add(new Task(request));
                 printReply("added: " + request);
             }
         }
         sc.close();
     }
 
+    // ACTIONS TO CALL TO A TASK
+    private void markTask(Task task, boolean b) {
+        if (b) {
+            task.mark();
+            printReply(
+                    "Nice! I've marked this task as done:",
+                    "\t" + task
+            );
+        } else {
+            task.unmark();
+            printReply(
+                    "OK, I've marked this task as not done yet:",
+                    "\t" + task
+            );
+        }
+    }
+    // ALL PRINT STATEMENTS
     private void listItems() {
         printDottedLine();
-        for (int i = 1; i <= items.size(); i++)
-            print(i + ". " + items.get(i - 1));
+        for (int i = 1; i <= tasks.size(); i++)
+            print(i + ". " + tasks.get(i - 1));
         printDottedLine();
     }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    private String message;
+    private boolean complete;
+
+    public Task(String message) {
+        this.message = message;
+        this.complete = false;
+    }
+
+    public void mark() {
+        this.complete = true;
+    }
+
+    public void unmark() {
+        this.complete = false;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public String getStatusIcon() {
+        return (complete ? "X" : " "); // mark done task with X
+    }
+
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.getMessage());
+    }
+}
