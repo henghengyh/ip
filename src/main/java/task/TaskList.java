181b4dc72052332440bd639887f91b64597be7ea
henghengyh <yihenglee6@gmail.com>
2026-01-29 10:25:40 +0800

    Add TaskList class without refactor

---
+++ src/main/java/task/TaskList.java
@@ -0,0 +1,32 @@
+package task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void add(Task t) {
+        tasks.add(t);
+    }
+
+    public Task remove(int index) {
+        return tasks.remove(index);
+    }
+
+    public Task get(int index) {
+        return tasks.get(index);
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public List<Task> getAll() {
+        return tasks;
+    }
+}
