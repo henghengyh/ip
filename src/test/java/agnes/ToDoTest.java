3b30a4dbd7558b75764dd59e6509f06f36d1adff
henghengyh <yihenglee6@gmail.com>
2026-01-29 16:29:11 +0800

    Create a trivial test suite ToDoTest

---
+++ src/test/java/agnes/ToDoTest.java
@@ -0,0 +1,28 @@
+package agnes;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import agnes.task.ToDo;
+
+// This is a trivial test suite
+public class ToDoTest {
+    @Test
+    public void messageTest() {
+        String message = "This is a sample message";
+        ToDo t = new ToDo(message);
+
+        String expected = "This is a sample message";
+        String actual = t.getMessage();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void markTest() {
+        ToDo t = new ToDo("");
+        t.mark();
+
+        String expected = "X";
+        String actual = t.getStatusIcon();
+        assertEquals(expected, actual);
+    }
+}
