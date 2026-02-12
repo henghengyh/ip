04ebcb3d54ce76cfa793bc4b3aab835546c85b19
henghengyh <yihenglee6@gmail.com>
2026-02-12 20:34:58 +0800

    Add update method for ToDo

--- src/main/java/agnes/task/ToDo.java
+++ src/main/java/agnes/task/ToDo.java
@@ -1,5 +1,9 @@
 package agnes.task;
 
+import java.time.format.DateTimeParseException;
+
+import agnes.util.DateTimeUtil;
+
 /**
  * Represents a ToDo task with a description, completion status.
  * <p>
@@ -23,5 +27,26 @@ public class ToDo extends Task {
                 "T | %s",
                 super.toFileFormat()
         );
+    }
+
+    /**
+     * Updates a field of a {@code ToDo}.
+     *
+     * @param field    The field to be updated.
+     * @param value    The value to be placed to the field.
+     * @throws IllegalArgumentException If the field is invalid.
+     */
+    @Override
+    public void update(String field, String value) throws IllegalArgumentException {
+        switch (field.toLowerCase()) {
+        case "/description":
+            this.setMessage(value);
+            break;
+        default:
+            throw new IllegalArgumentException(
+                    "Unknown field for event: " + field
+                            + "Available fields: /description"
+            );
+        }
     }
 }
