b4b4f57e6a95ef7ecfee948acb62561e520c9d32
henghengyh <yihenglee6@gmail.com>
2026-02-19 15:33:29 +0800

    insert app title in GUI

--- src/main/java/agnes/ui/Main.java
+++ src/main/java/agnes/ui/Main.java
@@ -23,6 +23,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Agnes - Yi Heng's self-designed chatbot <3");
+
             setWindowDimensions(stage);
             fxmlLoader.<MainWindow>getController().setAgnes(agnes); // inject the Agnes instance
             stage.show();
